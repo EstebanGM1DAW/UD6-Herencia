4ebed7cfe84bb9b444fc4c2a732f6cf3886c2021
EstebanGM1DAW <egonmorb@g.educaand.es>
2025-02-25 09:39:26 +0100

    cambios en libro

--- src/Biblioteca/Libro.java
+++ src/Biblioteca/Libro.java
@@ -1,27 +1,56 @@
 package Biblioteca;
 
-public class Libro extends Publicacion{
+interface Prestamo {
+    public void presta();
+
+    public void devuelve();
+
+    public boolean estaPrestado();
+
+}
+
+public class Libro extends Publicacion {
     boolean prestado;
-    public Libro(String codigoISBN, String titulo, int añoPublicacion){
-        super(codigoISBN,titulo,añoPublicacion);
+
+    public Libro(String codigoISBN, String titulo, int añoPublicacion) {
+        super(codigoISBN, titulo, añoPublicacion);
         this.prestado = false;
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         String pres = "no prestado";
         if (prestado) {
             pres = "prestado";
         }
-        return "ISBN: "+codigoISBN+", titulo: "+this.titulo+", año de publicacion: "+añoPublicacion+" ("+pres+")";
+        return "ISBN: " + codigoISBN + ", titulo: " + this.titulo + ", año de publicacion: " + añoPublicacion + " ("
+                + pres + ")";
     }
+
     public boolean isPrestado() {
         return prestado;
     }
+
     public void setPrestado(boolean prestado) {
         this.prestado = prestado;
     }
-    
-    public void prestar(){
 
+    public void presta() {
+        if (prestado == false) {
+            prestado = true;
+        } else {
+            System.out.println("Lo siento, Ese libro ya esta prestado");
+        }
+    }
+
+    public void devuelve() {
+        if (prestado == false) {
+        } else {
+            prestado = false;
+        }
+    }
+
+    public boolean estaPrestado(){
+        return prestado;
     }
 }
